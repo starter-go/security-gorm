9bde611b9f7389ab098b4f1ecb956ab6827543e4
MEZ <mez@bitwormhole.com>
2023-10-28 17:46:16 +0800

    调整了 update permissions 的作用字段

--- internal/implservice/permission_service_impl.go
+++ internal/implservice/permission_service_impl.go
@@ -41,13 +41,18 @@ func (inst *PermissionServiceImpl) Insert(c context.Context, o *rbac.PermissionD
 }
 
 // Update ...
-func (inst *PermissionServiceImpl) Update(c context.Context, id rbac.PermissionID, o *rbac.PermissionDTO) (*rbac.PermissionDTO, error) {
+func (inst *PermissionServiceImpl) Update(c context.Context, id rbac.PermissionID, o1 *rbac.PermissionDTO) (*rbac.PermissionDTO, error) {
+	o2, err := inst.PermissionConvertor.ConvertD2E(c, o1)
+	if err != nil {
+		return nil, err
+	}
 	o3, err := inst.PermissionDao.Update(nil, id, func(ent *rbacdb.PermissionEntity) {
-		// todo ...
-		ent.Method = o.Method
-		ent.Path = o.Path
-		ent.Resource = o.Method + ":" + o.Path
-		ent.AcceptRoles = o.AcceptRoles
+		// 不支持修改这些字段：method, path, resource
+		// ent.Method = o.Method
+		// ent.Path = o.Path
+		// ent.Resource = o.Method + ":" + o.Path
+		ent.AcceptRoles = o2.AcceptRoles
+		ent.Enabled = o2.Enabled
 	})
 	if err != nil {
 		return nil, err
