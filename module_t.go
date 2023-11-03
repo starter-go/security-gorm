506082df2b8267198e7e7695d7e5da6e457ac5ab
MEZ <mez@bitwormhole.com>
2023-11-03 16:13:07 +0800

    up to v0.0.14

--- module_t.go
+++ module_t.go
@@ -12,8 +12,8 @@ import (
 
 const (
 	theModuleName        = "github.com/starter-go/security-gorm"
-	theModuleVersion     = "v0.0.13"
-	theModuleRevision    = 13
+	theModuleVersion     = "v0.0.14"
+	theModuleRevision    = 14
 	theModuleResPath     = "src/main/resources"
 	theTestModuleResPath = "src/test/resources"
 )
