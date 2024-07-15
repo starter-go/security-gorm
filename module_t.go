7aac1330274719e4a0f3b5938553bc2d8dc8c92a
xukun <xukun17@sina.com>
2024-07-15 11:53:11 +0800

    up to v1.0.49

--- module_t.go
+++ module_t.go
@@ -12,8 +12,8 @@ import (
 
 const (
 	theModuleName     = "github.com/starter-go/security-gorm"
-	theModuleVersion  = "v1.0.48"
-	theModuleRevision = 21
+	theModuleVersion  = "v1.0.49"
+	theModuleRevision = 22
 )
 
 ////////////////////////////////////////////////////////////////////////////////
